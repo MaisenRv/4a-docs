b998a079a050100d071f2551b52b5f23211bb847
MaisenRv <dsmr19961@gmail.com>
2021-12-06 18:36:53 -0500

    modelo de transaccion

---
+++ products_ms/src/main/java/com/misiontic/products_ms/models/TransactionModel.java
@@ -0,0 +1,111 @@
+package com.misiontic.products_ms.models;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+
+public class TransactionModel {
+    @Id
+    private String id;
+    private String tipoTransaccion;
+    private String producto;
+    private Integer cantidad;
+    private String total;
+    private Date fecha;
+
+    public TransactionModel(String id,String tipoTransaccion,String producto,Integer cantidad,String total,Date fecha){
+        this.id = id;
+        this.tipoTransaccion = tipoTransaccion;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.total = total;
+        this.fecha = fecha;
+    }
+
+    
+
+    /**
+     * @return String return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the tipoTransaccion
+     */
+    public String getTipoTransaccion() {
+        return tipoTransaccion;
+    }
+
+    /**
+     * @param tipoTransaccion the tipoTransaccion to set
+     */
+    public void setTipoTransaccion(String tipoTransaccion) {
+        this.tipoTransaccion = tipoTransaccion;
+    }
+
+    /**
+     * @return String return the producto
+     */
+    public String getProducto() {
+        return producto;
+    }
+
+    /**
+     * @param producto the producto to set
+     */
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    /**
+     * @return Integer return the cantidad
+     */
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    /**
+     * @param cantidad the cantidad to set
+     */
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    /**
+     * @return String return the total
+     */
+    public String getTotal() {
+        return total;
+    }
+
+    /**
+     * @param total the total to set
+     */
+    public void setTotal(String total) {
+        this.total = total;
+    }
+
+    /**
+     * @return Date return the fecha
+     */
+    public Date getFecha() {
+        return fecha;
+    }
+
+    /**
+     * @param fecha the fecha to set
+     */
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+}
