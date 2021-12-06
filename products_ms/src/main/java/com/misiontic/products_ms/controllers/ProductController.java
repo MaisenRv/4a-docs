5542d080d73b600d4fe4c9032a1e71ee1ab86e7b
MaisenRv <dsmr19961@gmail.com>
2021-12-06 13:39:21 -0500

    controlador de producto con put y get de todos los rpoductos

--- products_ms/src/main/java/com/misiontic/products_ms/controllers/ProductController.java
+++ products_ms/src/main/java/com/misiontic/products_ms/controllers/ProductController.java
@@ -2,12 +2,16 @@ package com.misiontic.products_ms.controllers;
 
 import com.misiontic.products_ms.repositories.ProductRepository;
 import com.misiontic.products_ms.models.ProductModel;
+
+import java.util.List;
+
 import com.misiontic.products_ms.exceptions.ProductNotFoundException;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -19,13 +23,33 @@ public class ProductController {
         this.productRepository = productRepository; 
     }
 
-    @GetMapping("/product/{id}")
-    ProductModel getProduct(@PathVariable String id){
-        return productRepository.findById(id).orElseThrow(()-> new ProductNotFoundException("El producto no existe"));
+    @GetMapping("/product/{nombre}")
+    ProductModel getProduct(@PathVariable String nombre){
+        return productRepository.findById(nombre).orElseThrow(()-> new ProductNotFoundException("El producto no existe"));
     }
 
     @PostMapping("/product")
     ProductModel newProduct(@RequestBody ProductModel product) {
         return productRepository.save(product);
+    }
+
+    @PutMapping("/updateProduct/{nombre}")
+    ProductModel updateProduct (@RequestBody ProductModel productUpdate, @PathVariable String nombre){
+        
+        ProductModel product = productRepository.findById(nombre).orElseThrow(()-> new ProductNotFoundException("El producto no existe"));
+
+        product.setNombre(productUpdate.getNombre());
+        product.setDescripcion(productUpdate.getDescripcion());
+        product.setCantidad(productUpdate.getCantidad());
+        product.setTalla(productUpdate.getTalla());
+        product.setTipo(productUpdate.getTipo());
+
+        return productRepository.save(product);
+    }
+
+    @GetMapping("/products")
+    List<ProductModel> allProducts(){
+        List<ProductModel> allProducts = productRepository.findAll();
+        return allProducts;
     }
 }
