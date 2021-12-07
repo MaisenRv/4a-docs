7df73e7aee184bab1b3d30334447ac4208eebc20
MaisenRv <dsmr19961@gmail.com>
2021-12-07 11:41:46 -0500

    type defs de transaction

---
+++ Api_gateway/src/typeDefs/transaction_type_defs.js
@@ -0,0 +1,36 @@
+const {gql} = require("apollo-server");
+
+const transactionTypeDefs = gql 
+`
+type Transaction{
+    id: String!
+    usuarioOrigen: String!
+    usuarioDestino: String!
+    tipoTransaccion: String!
+    producto: String!
+    cantidad: Int!
+    total: String!
+    fecha: String!
+}
+
+input TransactionInput{
+    usuarioOrigen: String!
+    usuarioDestino: String!
+    tipoTransaccion: String!
+    producto: String!
+    cantidad: Int!
+    total: String!
+    fecha: String!
+}
+
+extend type Query{
+    transactionById(id: String!): Transaction
+}
+
+extend type Mutation{
+    createTransaction(trasaction: TransactionInput!): Transaction
+}
+
+`;
+
+module.exports = transactionTypeDefs;
